256c6f91e12af6a692359712ef6accc8982af3d7
BrunoOrzari <brunoorzari@gmail.com>
2025-11-10 13:20:50 -0300

    Update main.cpp

--- PDS/VPL_12/main.cpp
+++ PDS/VPL_12/main.cpp
@@ -1,1 +1,11 @@
+// NÃO ALTERE ESSA LINHA
+#include "avaliacao_basica_streaming.hpp"
 
+int main() {
+
+    //
+    // Adicione seu código aqui e faça as demais alterações necessárias
+    //
+
+    return 0;
+}
